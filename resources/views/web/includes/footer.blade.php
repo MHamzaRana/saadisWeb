4b50d2842119a7623eba38236a921ff90abd1060
MHamzaRana <muhammadhamza089@gmail.com>
2023-08-08 23:11:48 +0500

    custom.css version correction

--- resources/views/web/includes/footer.blade.php
+++ resources/views/web/includes/footer.blade.php
@@ -51,7 +51,7 @@
 <!-- sidebar -->
 <script src="{{asset('theme/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
 <script src="{{asset('theme/js/custom.js')}}"></script>
-<script src="{{asset('js/custom.js?n=0.1')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
 <script>
    function openNav() {
       document.getElementById("mySidenav").style.width = "250px";
