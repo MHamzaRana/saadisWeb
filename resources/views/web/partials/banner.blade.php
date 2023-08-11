69881d6e949fdb4a4ceabcfa0e0f85fdc8cd0585
MHamzaRana <muhammadhamza089@gmail.com>
2023-08-11 17:04:25 +0500

    delivery charges exampted in limited offer banner added

--- resources/views/web/partials/banner.blade.php
+++ resources/views/web/partials/banner.blade.php
@@ -5,24 +5,24 @@
                 <div class="carousel-item active">
                     <div class="row">
                         <div class="col-sm-12">
-                            <h1 class="banner_taital">Get Start <br>With New Arrival</h1>
-                            <div class="buynow_bt"><a href="{{route('explore','new-arrival')}}">Buy Now</a></div>
+                            <h1 class="banner_taital">📢 LIMITED TIME OFFER 📢 <br>❗ALL Pakistan Home Delivery Free❗</h1>
+                            <div class="buynow_bt"><a href="{{route('explore','new-arrival')}}">Buy New Arrival</a></div>
                         </div>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div class="row">
                         <div class="col-sm-12">
-                            <h1 class="banner_taital">Get Start <br>With Bin Saeed</h1>
-                            <div class="buynow_bt"><a href="{{route('explore','bin-saeed')}}">Buy Now</a></div>
+                            <h1 class="banner_taital">📢 LIMITED TIME OFFER 📢 <br>❗ALL Pakistan Home Delivery Free❗</h1>
+                            <div class="buynow_bt"><a href="{{route('explore','bin-saeed')}}">Buy Bin Saeed</a></div>
                         </div>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div class="row">
                         <div class="col-sm-12">
-                            <h1 class="banner_taital">Get Start <br>With aalaya</h1>
-                            <div class="buynow_bt"><a href="{{route('explore','aalaya')}}">Buy Now</a></div>
+                            <h1 class="banner_taital">📢 LIMITED TIME OFFER 📢 <br>❗ALL Pakistan Home Delivery Free❗❗❗</h1>
+                            <div class="buynow_bt"><a href="{{route('explore','aalaya')}}">Buy Aalaya</a></div>
                         </div>
                     </div>
                 </div>
