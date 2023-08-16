5f80b63f6d209be9ccec150c7468f8b414f26591
MHamzaRana <muhammadhamza089@gmail.com>
2023-08-16 23:04:29 +0500

    title to label in products card

--- resources/views/web/pages/explore.blade.php
+++ resources/views/web/pages/explore.blade.php
@@ -21,7 +21,7 @@
                                     <h4 class="shirt_text">{{$new->name}}</h4>
                                     <p class="price_text">Price <s style="color: #969696;">Rs. {{$new->st_price}}</s><br><span style="color: #262626;"> Rs. {{$new->price}}</span></p>
                                     <div class="tshirt_img">
-                                        <img class="product-img" title="{{$new->title}}" src="{{env('ADMIN_URL') .'/uploads/images/products/'. $new->images[0]->path}}">
+                                        <img class="product-img" title="{{$new->label}}" src="{{env('ADMIN_URL') .'/uploads/images/products/'. $new->images[0]->path}}">
                                         <img class="play-button" onclick="playVideo('{{$new->video[0]->url}}');" data-url="{{$new->video[0]->url}}" src="{{asset('theme/images/play-button.png')}}" data-toggle="modal" data-target=".video-modal-lg">
 
                                         <!-- <iframe width="100%" height="500" src="https://www.youtube.com/embed/HyQWr89JVEg"
--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -18,7 +18,7 @@
                                     <h4 class="shirt_text">{{$new->name}}</h4>
                                     <p class="price_text">Price <s style="color: #969696;">Rs. {{$new->st_price}}</s><br><span style="color: #262626;"> Rs. {{$new->price}}</span></p>
                                     <div class="tshirt_img">
-                                        <img class="product-img" title="{{$new->title}}" src="{{env('ADMIN_URL') .'/uploads/images/products/'. $new->images[0]->path}}">
+                                        <img class="product-img" title="{{$new->label}}" src="{{env('ADMIN_URL') .'/uploads/images/products/'. $new->images[0]->path}}">
                                         <img class="play-button" onclick="playVideo('{{$new->video[0]->url}}');" data-url="{{$new->video[0]->url}}" src="{{asset('theme/images/play-button.png')}}" data-toggle="modal" data-target=".video-modal-lg">
 
                                         <!-- <iframe width="100%" height="500" src="https://www.youtube.com/embed/HyQWr89JVEg"
@@ -72,7 +72,7 @@
                                     <h4 class="shirt_text">{{$BSitem->name}}</h4>
                                     <p class="price_text">Price <s style="color: #969696;">Rs. {{$BSitem->st_price}}</s><br><span style="color: #262626;"> Rs. {{$BSitem->price}}</span></p>
                                     <div class="tshirt_img">
-                                        <img class="product-img" title="{{$BSitem->title}}" src="{{env('ADMIN_URL') .'/uploads/images/products/'. $BSitem->images[0]->path}}">
+                                        <img class="product-img" title="{{$BSitem->label}}" src="{{env('ADMIN_URL') .'/uploads/images/products/'. $BSitem->images[0]->path}}">
                                         <img class="play-button" onclick="playVideo('{{$BSitem->video[0]->url}}');" data-url="{{$BSitem->video[0]->url}}" src="{{asset('theme/images/play-button.png')}}" data-toggle="modal" data-target=".video-modal-lg">
 
                                         <!-- <iframe width="100%" height="500" src="https://www.youtube.com/embed/HyQWr89JVEg"
@@ -126,7 +126,7 @@
                                     <h4 class="shirt_text">{{$ALitem->name}}</h4>
                                     <p class="price_text">Price <s style="color: #969696;">Rs. {{$ALitem->st_price}}</s><br><span style="color: #262626;"> Rs. {{$ALitem->price}}</span></p>
                                     <div class="tshirt_img">
-                                        <img class="product-img" title="{{$ALitem->title}}" src="{{env('ADMIN_URL') .'/uploads/images/products/'. $ALitem->images[0]->path}}">
+                                        <img class="product-img" title="{{$ALitem->label}}" src="{{env('ADMIN_URL') .'/uploads/images/products/'. $ALitem->images[0]->path}}">
                                         <img class="play-button" onclick="playVideo('{{$ALitem->video[0]->url}}');" data-url="{{$ALitem->video[0]->url}}" src="{{asset('theme/images/play-button.png')}}" data-toggle="modal" data-target=".video-modal-lg">
 
                                         <!-- <iframe width="100%" height="500" src="https://www.youtube.com/embed/HyQWr89JVEg"
