94d2fb65c0df1848f624596690766173d455792e
MHamzaRana <muhammadhamza089@gmail.com>
2023-09-07 03:54:01 +0500

    video width increased

--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -19,8 +19,9 @@
                                     <p class="price_text">Price <s style="color: #969696;">Rs. {{$new->st_price}}</s><br><span style="color: #262626;"> Rs. {{$new->price}}</span></p>
                                     <div class="tshirt_img">
                                         <!-- <img class="product-img" title="{{$new->label}}" src="{{env('ADMIN_URL') .'/uploads/images/products/'. $new->images[0]->path}}" alt="{{$new->label}}"> -->
-                                        <iframe width="267" height="476" class="prod-video" src="{{url($new->video[0]->url)}}" sandbox="allow-forms allow-scripts allow-pointer-lock allow-same-origin allow-top-navigation" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true">
+                                        <iframe width="300" height="476" class="prod-video" src="{{url($new->video[0]->url)}}" sandbox="allow-forms allow-scripts allow-pointer-lock allow-same-origin allow-top-navigation" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true">
                                         </iframe>
+                                        <!-- iframe width is 267 -->
                                         <!-- <img class="play-button" onclick="playVideo('{{$new->video[0]->url}}');" data-url="{{$new->video[0]->url}}" src="{{asset('theme/images/play-button.png')}}" data-toggle="modal" data-target=".video-modal-lg" alt="Paly"> -->
 
                                         <!-- <iframe width="100%" height="500" src="https://www.youtube.com/embed/HyQWr89JVEg"
